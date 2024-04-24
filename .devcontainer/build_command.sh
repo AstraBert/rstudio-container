8c5f623c79c03c76c58e8c1b08caf515bdaee10b
AstraBert <astraberte9@gmail.com>
2024-04-24 15:08:47 +0200

    /usr/local change

--- .devcontainer/build_command.sh
+++ .devcontainer/build_command.sh
@@ -2,10 +2,9 @@ docker buildx build \
 --label org.opencontainers.image.title=rstudio-container \
 --label org.opencontainers.image.description='RStudio Docker-based container' \
 --label org.opencontainers.image.url=https://github.com/AstraBert/rstudio-container \
---label org.opencontainers.image.source=https://github.com/AstraBert/rstudio-container --label org.opencontainers.image.version=0.0.0 \
---label org.opencontainers.image.created=2024-04-23T11:39:11.393Z \
+--label org.opencontainers.image.source=https://github.com/AstraBert/rstudio-container --label org.opencontainers.image.version=0.1.0 \
+--label org.opencontainers.image.created=2024-04-24T11:39:11.393Z \
 --label org.opencontainers.image.licenses=MIT \
 --platform linux/amd64 \
 --tag ghcr.io/astrabert/rstudio-container:latest \
---tag ghcr.io/astrabert/rstudio-container:0.0.0 \
 --push .
